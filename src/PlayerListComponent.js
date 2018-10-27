f062b5cb874355b33c8800800cf3b6ba2c02373a
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-10-26 18:54:57 -1000

    Did Timer

--- src/PlayerListComponent.js
+++ src/PlayerListComponent.js
@@ -3,12 +3,37 @@ import './App.css';
 import Player from './PlayerComponent';import Counter from './CounterComponent';
 
 class PlayerList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timer: null,
+      count: 60
+    }
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    let timer = setInterval(this.tick, 1000);
+    this.setState({timer});
+  }
+
+  componentWillUnmount() {
+    this.clearInterval(this.state.timer);
+  }
+
+  tick() {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
   render() {
     return (
       <div className="PlayerList">
         <div className='CodeCounter'>
         <h1>Put Code Here</h1>
-        <Counter/>
+        {/* <Counter seconds={this.state.seconds}/> */}
+        {this.state.count}
         </div>
         <h2>Players</h2>
         <Player/>
