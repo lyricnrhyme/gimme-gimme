23d9752f2c602ac073d7d977dce4ab2fee6b1a3c
Sarah <yamashige.sarah@gmail.com>
2018-10-28 12:12:02 -1000

    Fixing the joins to rooms

--- server/routes/Rooms.js
+++ server/routes/Rooms.js
@@ -91,7 +91,7 @@ router.route('/:id')
       rooms.map(room => {
         if (room.roomID === roomID) {
           let nameCheck = room.players.some(player => player.name === playerName)
-          if (nameCheck) {
+          // if (nameCheck) {
             room.players.push({
               name: playerName,
               score: 0
@@ -99,11 +99,11 @@ router.route('/:id')
             return res.json({
               roomID: room.roomID
             })
-          } else {
-            return res.json({ message: `Player name taken in this room!` })
-          }
-        } else {
-          return res.json({ message: `Room doesn't exist!` })
+        //   } else {
+        //     return res.json({ message: `Player name taken in this room!` })
+        //   }
+        // } else {
+        //   return res.json({ message: `Room doesn't exist!` })
         }
       })
     }  
