47412e72eebe3ebe14da5f5e309e003b0aad618c
Nathan N <Nitharios@users.noreply.github.com>
2018-10-28 09:55:26 -1000

    add processing image indicator to photo submit screen

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const io = socket(app);
 io.on('connection', socket => {
   socket.on('CREATE', data => {
     socket.join(data.roomID);
-    let countdown = 10;
+    let countdown = 30;
 
     const timer = setInterval(() => {
       io.to(data.roomID).emit('TICK', countdown)
@@ -48,11 +48,9 @@ io.on('connection', socket => {
   })
 
   socket.on('START_GAME', startData => {
-    // console.log('START', startData);
-
     socket.join(startData.roomID)
     io.to(startData.roomID).emit('PROMPT', generatePrompt())
-    let countdown = 10;
+    let countdown = 30;
 
     const timer = setInterval(() => {
       io.to(startData.roomID).emit('TICK', countdown)
--- src/containers/Camera/CameraComponent.js
+++ src/containers/Camera/CameraComponent.js
@@ -9,7 +9,8 @@ class Camera extends Component {
     this.state = {
       photo: null,
       badImage: false,
-      isDisabled: false
+      isDisabled: false,
+      submitted: false
     }
   }
 
@@ -20,6 +21,10 @@ class Camera extends Component {
 
   submitPhoto = e => {
     e.preventDefault();
+    this.setState({
+      submitted: true
+    })
+
     if (this.state.photo) {
       const form = new FormData();
       form.append('photo', this.state.photo);
@@ -34,7 +39,11 @@ class Camera extends Component {
               userName: this.props.user
             })
           } else {
-            this.setState({ badImage: true, isDisabled: true });
+            this.setState({
+              submitted: false,
+              badImage: true,
+              isDisabled: true
+            })
           }
         })
     }
@@ -46,6 +55,11 @@ class Camera extends Component {
         {
           this.state.badImage
             ? <span>Not what I wanted! Show me something else!</span>
+            : null
+        }
+        {
+          this.state.submitted
+            ? <span>Processing your image...</span>
             : null
         }
         <form>
