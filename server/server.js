fb0f401edab4c363f5d797b7c3df42cc7936e961
Nathan N <Nitharios@users.noreply.github.com>
2018-10-27 14:36:53 -1000

    deployment

--- server/server.js
+++ server/server.js
@@ -9,15 +9,15 @@ const routes = require('./routes');
 
 const PORT = process.env.PORT || process.env.EXPRESS_CONTAINER_PORT || 8989;
 
-// server.use(express.static(path.join(__dirname, '..', 'build')));
+server.use(express.static(path.join(__dirname, '..', 'build')));
 server.use(cors());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }))
 
 server.use('/api', routes);
-// server.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-// });
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 const app = server.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`)
